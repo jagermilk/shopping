fee75de0561151aa61aa0eb146d1935c9ded0702
“jagermilk” <463116435@qq.com>
2022-09-25 02:29:30 +0800

    detail参数

--- src/network/detail.js
+++ src/network/detail.js
@@ -29,3 +29,10 @@ export class ShopInfo{
         this.goodsCount=shopInfo.goodsCount
     }
 }
+export class GoodsParam{
+    constructor(info,rule){
+        this.image=info.images ? info.images[0]:'',
+        this.infos=info.set
+        this.sizes=rule.tables
+    }
+}
